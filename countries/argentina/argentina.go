720ccd5022d25057e444389a31c5bb67651343b3
LTNs35 <migueltg352340@gmail.com>
2021-10-10 16:37:17 +0200

    Added Argentina validation.

---
+++ countries/argentina/argentina.go
@@ -0,0 +1,33 @@
+package argentina
+
+import (
+	"github.com/ltns35/go-vat/countries"
+)
+
+type argentina struct {
+	countries.Country
+}
+
+var VAT = argentina{
+	Country: countries.Country{
+		Name: "Argentina",
+		Codes: []string{
+			"AR",
+			"ARG",
+			"032",
+		},
+		Rules: countries.CountryRules{
+			Regex: []string{
+				"^(AR)([0-9]{11})$",
+			},
+		},
+	},
+}
+
+func (a argentina) Calc(vat string) bool {
+	return len(vat) == 11
+}
+
+func (a argentina) GetCountry() *countries.Country {
+	return &a.Country
+}
---
+++ countries/mocks/argentina.go
@@ -0,0 +1,17 @@
+package mocks
+
+var ArgentinaValidTests = []string{
+	"AR12345678901",
+	"AR52189012424",
+	"AR06841241758",
+	"AR58412844712",
+	"AR14805128510",
+	"AR12451920841",
+	"AR78581724812",
+}
+
+var ArgentinaInvalidTests = []string{
+	"AR123412",
+	"AR751290472",
+	"AR0965172831741",
+}
