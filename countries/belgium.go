650eebadf84dd292ae8ef81a8961279b22eff931
Hal Wang <hwang.dev@gmail.com>
2025-06-05 12:41:04 +0100

    Updated Belgium rule to allow 1-prefixed VAT

--- countries/belgium.go
+++ countries/belgium.go
@@ -3,8 +3,6 @@ package countries
 import (
 	"math"
 	"strconv"
-
-	"github.com/ltns35/go-vat/countries/utils"
 )
 
 type belgium struct {
@@ -21,7 +19,7 @@ var Belgium = belgium{
 		},
 		Rules: CountryRules{
 			Regex: []string{
-				"^(BE)(0?\\d{9})$",
+				"^(BE)([01]?\\d{9})$",
 			},
 		},
 	},
@@ -32,11 +30,6 @@ func (b belgium) Calc(vat string) bool {
 	newVat := vat
 	if len(vat) == 9 {
 		newVat = "0" + vat
-	}
-
-	numAtFirstIndex := utils.IntAt(newVat, 1)
-	if numAtFirstIndex == 0 {
-		return false
 	}
 
 	strNum := newVat[:8]
